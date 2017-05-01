f589057fd8b3b8cf49a540d12ca38cbfeebeee40
Wong <stephane.wong1@gmail.com>
2017-05-01 02:55:28 +0200

    add pokemon -form

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,61 +2,83 @@ package controllers
 
 import javax.inject._
 
-import models.pokemons.PokeData
-import play.api._
+import models.pokemons.{Data, PokeData, PokemonForm, Sprites}
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(val ws: WSClient,configuration: play.api.Configuration) extends Controller {
+class HomeController @Inject()(val ws: WSClient, configuration: play.api.Configuration) extends Controller {
 
 
-
-  protected lazy val pokemons = fillPokemonData
-  protected lazy val poke = pokemons.map(_._1)
+  protected lazy val pokemons: Future[scala.collection.mutable.HashMap[String, String]] = fillPokemonData
+  protected lazy val poke = pokemons.map(_.map(_._1).toList)
   protected val url = configuration.underlying.getString("pokedex.url")
+  protected val pokemonUrl = url + "pokemon/"
+  protected val pokemonFormUrl = url + "pokemon-form/"
 
 
   def getAllPokemonName = Action.async { implicit request =>
-    val f = ws.url(url + "pokemon").get()
+    /*val f = ws.url(url + "pokemon").get()
     f.map{ ws =>
       val test = ws.json.validate[PokeData]
       val t = test.asOpt.map { pokeData =>
         pokeData
-      }.getOrElse(throw new Exception("data not found"))
-      Ok(Json.toJson(t))
-    }
-  }
-
-  protected def fillPokemonData: scala.collection.mutable.HashMap[String,String] = {
-    val map = scala.collection.mutable.HashMap[String,String]()
-
-    def fillData(url: String): Unit = {
-      ws.url(url).get().map { wsr =>
-        wsr.json.validate[PokeData].asOpt.map { pokeData =>
-          pokeData.results.map { data => map.put(data.name,data.url)}
-          pokeData.next.map { next => fillData(next)}
-        }
+      }.getOrElse(throw new Exception("data not found"))*/
+    poke.flatMap { l =>
+      getData(pokemonFormUrl + l(0)).map { wsr =>
+        println(wsr.json)
+        val pokemonForm = wsr.json.validate[PokemonForm].asOpt.getOrElse(throw new Exception("data not found"))
+        Ok(Json.toJson(pokemonForm))
       }
     }
-    map
+
+    //poke.map(i => Ok(Json.toJson(i)))
+    //Future.successful(Ok(Json.toJson(poke)))
+  }
+
+
+  protected def getData(url: String) = ws.url(url).get()
+
+
+  protected def fillPokemonData: Future[scala.collection.mutable.HashMap[String, String]] = {
+    val map = scala.collection.mutable.HashMap[String, String]()
+
+    def getCount(url: String): Future[Int] = {
+      ws.url(url).get().map { wsr =>
+        wsr.json.validate[PokeData].asOpt.map(_.count).getOrElse(0)
+      }
+    }
+
+    def fillData(url: String) = {
+      ws.url(url).get().map { wsr =>
+        wsr.json.validate[PokeData].asOpt.map { pokeData =>
+          pokeData.results.map { data => map.put(data.name, data.url) }
+        }
+        map
+      }
+    }
+
+    getCount(pokemonUrl).flatMap { count =>
+      fillData(url + "pokemon/?limit=" + count)
+    }
+
   }
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
--- app/models/pokemons/Pokemon.scala
+++ app/models/pokemons/Pokemon.scala
@@ -5,21 +5,16 @@ import play.api.libs.json.Json
 /**
   * Created by wong on 29/04/17.
   */
-case class Pokemon(id: String,
-                   name: String,
-                   base_experience: String,
-                   height: Int,
-                   is_default: Boolean,
-                   order: Int,
-                   weight: Int,
-                   abilities: List[Ability],
-                   forms: List[Data],
-                   game_indices: List[GameIndices]
+
+case class Sprites(back_default: Option[String],
+                   back_shiny: Option[String],
+                   front_default: Option[String],
+                   front_shiny: Option[String]
                   )
 
-case class GameIndices(game_index: Int,
-                       version: Data
-                      )
+object Sprites {
+  implicit val spritesFormat = Json.format[Sprites]
+}
 
 case class Data(name: String,
                 url: String)
@@ -27,6 +22,32 @@ case class Data(name: String,
 object Data {
   implicit val dataFormat = Json.format[Data]
 }
+
+case class DataName(name: String, language: Data)
+
+object DataName {
+  implicit val dataNameFormat = Json.format[DataName]
+}
+
+case class PokemonForm(id: Int,
+                       name: String,
+                       order: Int,
+                       form_order: Int,
+                       is_default: Boolean,
+                       is_battle_only: Boolean,
+                       is_mega: Boolean,
+                       form_name: String,
+                       pokemon: Data,
+                       sprites: Sprites,
+                       version_group: Data,
+                       names: List[DataName] = Nil,
+                       form_names: List[DataName] = Nil
+                      )
+
+object PokemonForm {
+  implicit val pokemonFormFormat = Json.format[PokemonForm]
+}
+
 
 case class Ability(id_hidden: Boolean,
                    slot: Int,
