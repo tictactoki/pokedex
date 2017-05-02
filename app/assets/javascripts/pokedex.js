fff7ec4b9e1c44b196fa8699e0ef9c21be8c7d1f
Wong <stephane.wong1@gmail.com>
2017-05-02 02:49:04 +0200

    can create tweet

--- app/assets/javascripts/pokedex.js
+++ app/assets/javascripts/pokedex.js
@@ -7,12 +7,27 @@ const elm = React.createElement;
 const Pokemon = React.createClass({
 
     render: function () {
-        return elm("a",{className: "twitter-timeline",href: "https://twitter.com/hashtag/pikachu", 'data-widget-id': "859164250522214400"},"tweet");
+        return elm("a",{className: "twitter-timeline",href: "https://twitter.com/hashtag/charizard", 'data-widget-id': "859164250522214400"},"tweet");
     }
 
 });
 
-ReactDOM.render(
+$(document).ready(function () {
+    twttr.widgets.createTimeline(
+        {
+            sourceType: 'profile',
+            screenName: 'charizard'
+        },
+        document.getElementById('content'),
+        {
+            width: '450',
+            height: '400',
+            related: 'charizard,twitterapi'
+        }).then(function (el) {
+        console.log('Embedded a timeline.')
+    });
+});
+/*ReactDOM.render(
   elm(Pokemon, null,null),
   document.getElementById("content")
-);
+);*/
